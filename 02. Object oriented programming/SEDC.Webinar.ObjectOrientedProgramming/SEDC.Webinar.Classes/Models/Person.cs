72931694f7e2649433e7df84433fe984b16b7685
Martin <panovski.martin93@gmail.com>
2020-06-21 13:27:46 +0200

    Object oriented programming webinar demo code added

---
+++ 02. Object oriented programming/SEDC.Webinar.ObjectOrientedProgramming/SEDC.Webinar.Classes/Models/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEDC.Webinar.Classes.Models
+{
+    //Simple declaration of class in C#
+    public class Person
+    {
+        //Properties (Data)
+        //Every instance of this class can have FirstName, LastName, Age.....
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Profession { get; set; }
+        private string Password { get; set; }
+
+
+        //Default constructor explicitly specified
+        //If we want to instantiate object with empty constructor but still we want to have 
+        //Different constructors for creating instances by using different constructor
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName, int age, string profession)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Profession = profession;
+        }
+
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Profession = "Not defined";
+        }
+
+
+        //Methods (Behavior)
+        //Every instance of this class can have different behavior like
+        //Every instanc can Eat, can Sleep, can Work
+        public void Info()
+        {
+            Console.WriteLine($"{FirstName} {LastName} is {Age} years old and its profession is {Profession}!");
+        }
+
+        public void Eat(string meal)
+        {
+            Console.WriteLine($"{FirstName} is eating {meal}");
+        }
+
+        public void Sleep(int from, int till)
+        {
+            Console.WriteLine($"{FirstName} is sleeping from {from} till {till}");
+        }
+
+        public string Work()
+        {
+            return $"{FirstName} is working hard!";
+        }
+
+
+        public bool SetPasswrod(string password)
+        {
+            if(password.Length == 0 || password.Length <= 5)
+            {
+                return false;
+            }
+            else
+            {
+                Password = password;
+                return true;
+            }
+        }
+    }
+}
---
+++ 02. Object oriented programming/SEDC.Webinar.ObjectOrientedProgramming/SEDC.Webinar.Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using SEDC.Webinar.Inheritance.Models;
+using System;
+
+namespace SEDC.Webinar.Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Inheritance
+            //Instances from Student class
+            Student std1 = new Student("Martin Panovski", 26, "070/345-554", "G2", Academy.WebDevelopment);
+            Student std2 = new Student("Dejan Jovanov", 27, "071/342-554", "G2", Academy.WebDesign);
+            Student std3 = new Student("Ivo Kostovski", 29, "071/335-554", "G2", Academy.Networks);
+
+            //Instances from Trainer class
+            Trainer trainer = new Trainer("Dragan Gelevski", 27, "070/234-998", 3);
+
+            std1.Info();
+            std2.Info();
+            std3.Info();
+
+
+            trainer.Info();
+
+            //Since student inherits from human we can create an instance from student 
+            //But the type of that instance to be Human
+            //Basically it is the same thing because Student inherits from Human!
+            Human std4 = new Student("Ivo Kostovski", 29, "071/335-554", "G2", Academy.Networks);
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+            Console.ReadLine();
+        }
+    }
+}
